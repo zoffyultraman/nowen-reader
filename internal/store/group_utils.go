e7d6711a3a21aa135b183bfcc0c184990bd6deef
cropflre <767308865@qq.com>
2026-04-09 10:24:07 +0800

    fix: 删除 group_utils.go 中重复的 package store 声明

--- internal/store/group_utils.go
+++ internal/store/group_utils.go
@@ -1,18 +1,5 @@
 package store
 
-package store
-
-import (
-	"database/sql"
-	"fmt"
-	"log"
-	"path"
-	"sort"
-	"strings"
-	"time"
-	"unicode"
-)
-
 // firstString 从可变参数中安全取第一个字符串值。
 func firstString(s []string) string {
 	if len(s) > 0 {
@@ -20,4 +7,3 @@ func firstString(s []string) string {
 	}
 	return ""
 }
-
